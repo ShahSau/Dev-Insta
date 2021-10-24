58431e0ea8700b53d08a0a7c73a363808b0d760b
shahriar <shahriarksaurov@gmail.com>
2021-10-25 01:44:20 +0300

    indentation

--- config/db.js
+++ config/db.js
@@ -1,18 +1,23 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI')
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
-const connectDB = async()=>{
-    try{
-        await mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false })
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
 
-        console.log('MongoDB connected')
-    }catch(err){
-        console.log(err.message)
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.log(err.message);
 
-        // Exit procees with failure
-        process.exit(1)
-    }
-} 
+    // Exit procees with failure
+    process.exit(1);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
